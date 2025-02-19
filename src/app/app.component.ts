734c1ee622c62cff3b997105dc66db4a47323c99
Gustavo Adolfo López Delgado <hust_mw2000@hotmail.com>
2025-02-18 23:04:09 -0600

    Sprint2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { MainComponent } from "./main/main.component";
+import { AsideComponent } from "./aside/aside.component";
+import { FooterComponent } from "./footer/footer.component";
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, MainComponent, AsideComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Sprint2';
+
 }
