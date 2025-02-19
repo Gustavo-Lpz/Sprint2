67d5c46f56e0152daf62bb2e0d37ecfbf2b5c6b3
Gustavo Adolfo López Delgado <hust_mw2000@hotmail.com>
2025-02-19 15:42:18 -0600

    Sprint2

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -4,11 +4,64 @@ import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-main',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './main.component.html',
   styleUrl: './main.component.css'
 })
 export class MainComponent {
 
+  datos = {
+    imageUrl:"",
+    titulo: "",
+    precio:"",
+    cantidad:"",
+    descripcion:"",
+  };
 
-}
+  onImageChange(event: Event) {
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.datos.imageUrl = reader.result as string;
+      };
+      reader.readAsDataURL(file); 
+    }
+  }
+
+  cancelarProducto() {
+    this.limpiarDatos();
+  }
+
+  private limpiarDatos() {
+    this.datos = {
+      imageUrl: '',
+      titulo: '',
+      precio: '',
+      cantidad: '',
+      descripcion: ''
+    };
+  }
+
+  
+  productos: any[] = [];
+
+  agregarProducto() {
+    if (this.datos.titulo && this.datos.precio && this.datos.cantidad && this.datos.descripcion && this.datos.imageUrl) {
+      this.productos.push({ ...this.datos }); 
+      this.datos = {
+        imageUrl: '',
+        titulo: '',
+        precio: '',
+        cantidad: '',
+        descripcion: ''
+      };
+    } else {
+      alert('Complete todos los campos');
+    }
+  }
+
+
+
+
+} /* Fin*/
